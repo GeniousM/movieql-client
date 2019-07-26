a9ca476447d7605f7f2b9e06bb875200580a12f4
Jaeyoung Song <sjy920404@naver.com>
2019-07-26 16:47:01 +0900

    follow Niccolas AWesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
 // function App() {
 //   return <div className="App" />;
@@ -10,7 +13,12 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App" />
+        <Router>
+          <React.Fragment>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:movieId"} component={Detail} />
+          </React.Fragment>
+        </Router>
       </ApolloProvider>
     );
   }
